b5d87b5710b8a5349866b1b84aeb7486c9ebd560
pooja <khatrip197@gmail.com>
2025-03-08 17:38:14 +0530

    adding modified version

--- admin2/load_service.php
+++ admin2/load_service.php
@@ -1,3 +1,5 @@
+
+
 <?php
 $servername = "localhost";
 $username = "root";
@@ -10,6 +12,7 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 // if (isset($_POST['request_type'] ==""))
+$request_type = isset($_POST['request_type']) ? $_POST['request_type'] : '';
 if($_POST['request_type']=="")
 {
     print_r($_POST);
@@ -25,7 +28,6 @@ while ($row = mysqli_fetch_assoc($query) )
  echo  $row['c_service'];
     $str.="<option value= '{$row['c_id']}'>{$row['c_service']}";
 }
-
 echo $str;
 }
 else
@@ -36,7 +38,7 @@ if($_POST['request_type']=="sub_service_data")
     $query = mysqli_query($conn,$sql);
     echo "pooja";
     // $str = '<option value="">Select Service</option>';
-    $str = "";
+    $str = '<option value="">Select Service</option>';
     while ($row = mysqli_fetch_assoc($query) )
     {
       echo $row['s_id'] ;
@@ -58,29 +60,51 @@ if (isset($_POST['sub_service'])) {
     $result = mysqli_query($conn, $query);
 
     if (mysqli_num_rows($result) > 0) {
-        echo "<table border='1' cellspacing='0' cellpadding='10'>
-                <tr>
+     
+        // echo "<table border='1' cellspacing='0' cellpadding='10'>
+        // <thead style='background-color: rgb(51, 139, 139);'>
+        //         <tr>
                 
-                    <th>Service Name</th>
-                    <th>Price</th>
-                    <th>service_number</th>
-                </tr>";
+        //             <th>Service Name</th>
+        //             <th>Price</th>
+        //             <th>service_number</th>
+        //              </thead>
+        //         </tr>";
+        echo '<div class="card-body">
+        <table id="example1" class="table table-bordered table-striped">
+            <thead style="background-color: rgb(51, 139, 139);">
+                <tr>
+                    <th style="color: rgb(238, 230, 217); font-weight: 500;">S No.</th>
+                    <th style="color: rgb(238, 230, 217); font-weight: 500;">Service Name</th>
+                    <th style="color: rgb(238, 230, 217); font-weight: 500;">Service Price</th>
+                      <th style="color: rgb(238, 230, 217); font-weight: 500;">Service Number</th>
+                       <th style="color: rgb(238, 230, 217); font-weight: 500;">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+            ';
 
+        $sno = 1;
         while ($row = mysqli_fetch_assoc($result)) {
             echo "<tr>
-                  
-                    <td>" . $row['all_service'] . "</td>
-                    <td>" . $row['price'] . "</td>
-                    <td>" . $row['service_number'] . "</td>
+                    <td>{$sno}</td>
+                    <td>{$row['all_service']}</td>
+                    <td>{$row['price']}</td>
+                     <td>{$row['service_number']}</td>
+                     <td> <i class='fas fa-pencil-alt' style='color:rgb(18, 110, 40);'></i> 
+                 
                   </tr>";
+            $sno++;
         }
 
-        echo "</table>";
+        // echo "</table>";
+        echo '  </tbody>
+        </table>
+      </div>';
     } else {
         echo "<p>No services found.</p>";
     }
 }
-
 ?>
 
 
--- admin2/manage_service.php
+++ admin2/manage_service.php
@@ -9,30 +9,20 @@ include('includes/top_navbar.php');
 include('includes/sidebar.php');
 ?>
 <?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "beauty";
-// $port = 3307;
-// $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
-
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
 include 'db_connection.php';
 ?>
-<?php
-$sql = "SELECT * FROM tb_services";
-?>
+
+<main class="app-main">
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BEAUTY PARLOUR MANAGEMENT SYSTEM</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <style type="text/css">
@@ -48,56 +38,26 @@ $sql = "SELECT * FROM tb_services";
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
+      <div class="row mb-2">
+      <div class="col-sm-6">
+      </div><!-- /.col -->
+      <div class="col-sm-6">
+      <ol class="breadcrumb float-sm-right">
+      <button class="btn" style="background-color:rgb(51, 139, 139);border: none; cursor: pointer;  padding: 7px 7px;">
+    <i class="fa fa-magic" style="margin-right: 2px; color: black; font-size: 14px;"></i>
+    <a href="#" class="text-white mx-1" data-toggle="modal" data-target="#modal-default"
+       style="text-decoration: none; color: black; font-size: 14px; font-weight: 650;  margin: 4px 2px;">
+      Add Services
+    </a>
+  </button>
+</ol>
+  </div><!-- /.col -->
+        </div><!-- /.row -->
       
-      </div><!-- /.container-fluid -->
+     
     </div>
     <div class="container-fluid">
       
-    <!-- <h4>MANAGE SERVICES</h4> -->
-  
-    <!-- <table class="table table-bordered">
-  <thead style="background-color:rgb(23, 162, 184); color: white;">
-    <tr>
-      <th scope="col">S.no</th>
-      <th scope="col">SERVICE NAME</th>
-      <th scope="col">SERVICE PRICE</th>
-      <th scope="col">ACTION</th>
-    </tr>
-  </thead>
-  <tbody> -->
-  <?php
-//   $sql = "SELECT * FROM tb_services";
-// // Step 3: Execute the query
-// $result = mysqli_query($conn, $sql);
-// $count = 0;
-// // Step 4: Check if the query returned any results
-// if (mysqli_num_rows($result) > 0) {
-//   $count = 0;
-//     // Step 5: Use a while loop to fetch each row of data
-//     while ($row = mysqli_fetch_assoc($result)) {
-//       $count=$count +1;
-//       echo"<tr>
-//       <th scope='row'>".$count."</th>
-//       <td>".$row['service_name']."</td>
-//       <td>".$row['service_price']."</td>  
-// <td> <a href='/beauty_parlour_management_system/admin2/edit_services.php?id={$row["id"]}'>
-//   <button style='background-color:rgb(23, 162, 184); color: white; border: none; padding: 5px 10px; cursor: pointer;'>Edit</button>
-// <a href='/beauty_parlour_management_system/admin2/delete_service.php?id={$row["id"]}'>
-//  <button style='background-color: #f44336; color: white; border: none; padding: 5px 10px; cursor: pointer;'> Delete </button></td>
-//     </tr>";
-        // // Step 6: Display the data (ID, service name, price, and creation date)
-        // echo "ID: " . $row['id'] . "<br>";
-        // echo "Service Name: " . $row['service_name'] . "<br>";
-        // echo "Service Price: $" . $row['service_price'] . "<br>";
-        // echo "Creation Date: " . $row['creation_date'] . "<br><br>";
-//     }
-// } else {
-//     echo "No services found.";
-// }
-// ?>
-<!-- //   </tbody>
-// </table>
-// </div> -->
 <div class="row">
                     <div class="col-sm-4">
                       <!-- select -->
@@ -105,9 +65,6 @@ $sql = "SELECT * FROM tb_services";
                         <label>Select Category</label>
                         <select class="form-control" id="service">
                         <option>Select </option>
-                          <!-- <option>Hair Services</option>
-                          <option>Beauty Services</option>
-                          <option>Hand & Feet Services</option> -->
                         </select>
                       </div>
                     </div>
@@ -118,24 +75,11 @@ $sql = "SELECT * FROM tb_services";
                         <label>Select Sub Category</label>
                         <select class="form-control"  id="sub_service">
                         <option>Select </option>
-                          <!-- <option>option 1</option>
-                          <option>option 2</option>
-                          <option>option 3</option>
-                          <option>option 4</option>
-                          <option>option 5</option> -->
                         </select>
                       </div>
                     </div>
-<div class="card">
-              <div class="card-header">  
-              <ol class="breadcrumb float-sm-right">
-  <button class="btn" style="background-color:rgb(51, 139, 139);border: none; cursor: pointer;  padding: 7px 7px;">
-    <i class="fa fa-magic" style="margin-right: 2px; color: black; font-size: 14px;"></i>
-    <a href="#" class="text-white mx-1" data-toggle="modal" data-target="#modal-default"
-       style="text-decoration: none; color: black; font-size: 14px; font-weight: 650;  margin: 4px 2px;">
-      Add Services
-    </a>
-  </button>
+
+  
   <div class="modal fade" id="modal-default">
         <div class="modal-dialog">
           <div class="modal-content">
@@ -168,75 +112,49 @@ $sql = "SELECT * FROM tb_services";
         </div>
         <!-- /.modal-dialog -->
       </div>
-</ol>
 
+</div>
 <!-- <div id="service_table"></div>             -->
                 <!-- <h3 class="card-title">Appointment Details</h3> -->
-                <h5 class="m-0">OUR SERVICES </h5>
-                
+              
+     <!-- <div class="card-body">      
+    <table id="example1" class="table table-bordered table-striped">
+       
+    </table>
+</div> -->
+              <!-- /.card-body -->
+     
+            <!-- /.card -->
+         
+          <!-- /.col -->
+       
+        <!-- /.row -->
+        <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Our services</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
                 <table id="example1" class="table table-bordered table-striped">
-                <!-- <thead style="background-color:rgb(33, 70, 77); color: white;"> -->
-                <thead style="background-color: rgb(51, 139, 139) ">
+                <thead style="background-color: rgb(51, 139, 139);">
                   <tr>
-                    <th style="color: rgb(238, 230, 217); font-weight: 500;">S no.</th>
-                    <th style="color: rgb(238, 230, 217); font-weight: 500;">Service name</th>
-                    <th style="color: rgb(238, 230, 217); font-weight: 500;">Service Price </th>
-                    <?php
- if($role==1)
-{
-?>       
-  <th style="color: rgb(238, 230, 217); font-weight: 500;">Actions</th> <?php } ?>
+                    <th>S no.</th>
+                    <th>Service name</th>
+                    <th>Service price</th>
+                    <th>Service number</th>
+                    <th>CSS grade</th>
                   </tr>
                   </thead>
                   <tbody>
-                  <?php
-  $sql = "SELECT * FROM all_services WHERE service_number = 1";
-// Step 3: Execute the query
-$result = mysqli_query($conn, $sql);
-$count = 0;
-// Step 4: Check if the query returned any results
-if (mysqli_num_rows($result) > 0) {
-  $count = 0;
-    while ($row = mysqli_fetch_assoc($result)) {
-      $count=$count +1;
-      ?>
-      <tr>
-      <th scope='row'><?php echo $count; ?></th>
-      <td><?php echo $row['all_service']; ?></td>
-      <td><?php echo $row['price']; ?>
-<!-- <td> <a href='/beauty_parlour_management_system/admin2/edit_services.php?id=<?php echo $row["id"]; ?>'>
-  <button style='background-color:rgb(23, 162, 184); color: white; border: none; padding: 5px 10px; cursor: pointer;'>Edit</button>
-<a href='/beauty_parlour_management_system/admin2/delete_service.php?id=<?php echo $row["id"]; ?>'>
- <button style='background-color: #f44336; color: white; border: none; padding: 5px 10px; cursor: pointer;'> Delete </button></td>
- <td> -->
-  <?php
- if($role==1)
-{
-?>
-  <td>
-    <div style="display: inline-block; margin-right: 20px;">
-        <a href='/beauty_parlour_management_system/admin2/edit_services.php?id=<?php echo $row["id"]; ?>'>
-            <i class='fas fa-pencil-alt' style='color:rgb(18, 110, 40);'></i> <!-- Edit icon -->
-        </a> 
-    </div>
-    <div style="display: inline-block;">
-        <a href='/beauty_parlour_management_system/admin2/delete_service.php?id=<?php echo $row["id"]; ?>'>
-            <i class='fa fa-trash' style='color: red;'></i> <!-- Trash icon -->
-        </a>
-    </div>
-</td>
-    </tr>
-       <?php } ?>
-        <?php
-    }
-} else {
-    echo "No services found.";
-}
-?>
-
+                  <tr>
+                    <td></td>
+                    <td>
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+</tbody>
                   </table>
               </div>
               <!-- /.card-body -->
@@ -251,6 +169,9 @@ if (mysqli_num_rows($result) > 0) {
     </section>
     <!-- /.content -->
   </div>
+      
+  
+
   <script>
         $(document).ready(function () {
             $("#submitBtn").click(function (e) {
@@ -274,25 +195,6 @@ if (mysqli_num_rows($result) > 0) {
             });
         });
     </script>
-    <!-- <script>
-$(document).ready(function()){
-function loadData()
-{
-$.ajax({
-
-  url : "load_service.php",
-  type : "POST",
-  success : function (data)
-  {
-    $("#service").append(data);
-  }
-});
- 
-loadData();
-}
-}
-
-      </script> -->
       <script>
 $(document).ready(function() {
     function loadData(request_type, category_id) {
@@ -305,9 +207,9 @@ $(document).ready(function() {
               if(request_type=="sub_service_data")
             {
               $("#sub_service").html(data); // Replace content instead of appending
-              console.log(data);
-              console.log("Sending request_type:", request_type);
-              console.log("Sending id:", category_id);
+              // console.log(data);
+              // console.log("Sending request_type:", request_type);
+              // console.log("Sending id:", category_id);
             }
             else{
               $("#service").html(data); // Replace content instead of appending
@@ -333,10 +235,8 @@ $(document).ready(function() {
     loadData("sub_service_data", service);
   }
   else{
-$("#state").html("");
+$("#service").html("");
   }
-
-
   })
   $("#sub_service").on("change",function()
   {
@@ -351,10 +251,12 @@ $("#state").html("");
             // $("#service").html(response); // Update service section with fetched data
             console.log("Received Data:", response);
             // $("#example1").html(response); 
-            var tableContent = $(response).find("table").html(); 
+            var tableContent = $(response).find(".card-body").html(); 
 
 if (tableContent) {
-    $("#example1").html("<table class='your-table-class'>" + tableContent + "</table>"); 
+    // $("#example1").html("<table class='table table-bordered table-striped'>" + tableContent + "</table>"); 
+    $("#example1").html('<div class="card-body">' + tableContent + '</div>'); 
+    
 } else {
     console.warn("No table data found in response.");
 }
@@ -364,8 +266,6 @@ if (tableContent) {
         }
     });
   })
-
-
 });
 </script>
   </body>
